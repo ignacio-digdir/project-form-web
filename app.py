361533ffbefe653c6f7b9b3657bd3e7e90381b31
ignacio-digdir <ignacio.cuervo.torre@digdir.no>
2025-06-06 12:52:29 +0200

    updated app files

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from app import create_app
-
+from flask import Flask
+# app = Flask(__name__)
 app = create_app()
 
 if __name__ == "__main__":
--- startup.sh
+++ startup.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-gunicorn --bind=0.0.0.0 --timeout 600 app:app
+gunicorn --bind=0.0.0.0 --timeout 600 app:create_app
